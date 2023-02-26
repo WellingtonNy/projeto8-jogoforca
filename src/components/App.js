3bf0b084b22316a4fe4c50cfd1c6f88a53ab50dc
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-02-26 09:08:45 -0300

    add palavra escondida

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,12 @@ export default function App(){
 
 const [numeroImg,setNumeroImg]=React.useState(0)
 const [palavra,setPalavra]=React.useState('')
+const [letrasChute,setLetrasChute]=React.useState([])
 
   return(
   <div className="container">
    <Jogo 
+   letrasChute={letrasChute}
    palavras={palavras}
    palavra={palavra}
    setPalavra={setPalavra}
--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,9 +1,10 @@
 export default function Jogo(props){
 
+    
+    
     function escolhaPalavra() {
         let numeroAleatorio= Math.floor(Math.random() * props.palavras.length)
         props.setPalavra(props.palavras[numeroAleatorio])
-        console.log(props.palavras[numeroAleatorio]);
     }
 
     return(
@@ -13,7 +14,10 @@ export default function Jogo(props){
          </div>
          <div className="palavra">
             <button onClick={escolhaPalavra} className="escolha">Escolher Palavra</button>
-            <h1 className="tracinho">_ a s d _ _ _ _ _ _ _</h1>
+            <h1 className="tracinho">{props.palavra.split('').map((l)=>
+            <span className="sLetra">
+            <span className={props.letrasChute.includes(l)? '' : 'hiden'}>{l}</span>
+            </span>)}</h1>
          </div>
         </div>
     );
